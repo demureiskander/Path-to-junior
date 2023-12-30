f72cfbc5d659938b7a1aca0c2469ac1d8448d8d9
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2023-12-30 15:00:02 +0300

    Add files via upload

---
+++ How_many_words.rb
@@ -0,0 +1,7 @@
+def ur_word(word, number)
+    print("Твое слово: ", word, " | Твоя цифра: ", number)
+end
+
+
+puts("Введи слово, нажми энтер, а потом введи цифру:")
+ur_word(word = gets.chomp, number = gets)
