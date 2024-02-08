870d4e40dc91edde8d41c8fc3fe4bd9f3d9181f9
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-02-08 22:04:16 +0300

    Add files via upload

---
+++ Your age, heigh and first letter.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char first_letter;
+	unsigned short age;
+	float height;
+
+	printf("Write the first letter of your name: ");
+	scanf_s("%c", &first_letter);
+	
+	printf("Write your age: ");
+	scanf_s("%d", &age);
+	
+	printf("Write your height: ");
+	scanf_s("%f", &height);
+
+	printf("The first letter of your name: %c | your age: %d | your height: %.2f \n", first_letter, age, height);
+
+	return 0;
+}
