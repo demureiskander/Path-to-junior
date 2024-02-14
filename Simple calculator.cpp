decf3c2c74fe274c7126294444192ff1729aebd9
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-02-15 01:59:54 +0300

    Add files via upload

---
+++ Simple calculator.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+int main() {
+	float n1, n2, res;
+	char mathop;
+
+	printf("please write first number: ");
+	scanf_s("%f", &n1);
+	printf("please write second number: ");
+	scanf_s("%f", &n2);
+	printf("please chose any math operation (+,-,*,/): ");
+	scanf_s(" %c", &mathop);
+
+	switch (mathop) {
+		case '+':
+			res = n1 + n2;
+			break;
+		case '-':
+			res = n1 - n2;
+			break;
+		case '*':
+			res = n1 * n2;
+			break;
+		case '/':
+			res = n1 / n2;
+			break;
+	}	
+
+	printf("%.2f", res);
+
+	return 0;
+}
