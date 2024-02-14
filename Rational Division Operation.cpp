c466cd767f58d60ab75f4110e765f6d9e6f3973d
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-02-15 00:44:51 +0300

    Add files via upload

---
+++ Rational Division Operation.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+	float n1, n2, result;
+
+	printf("Print number n1: ");
+	scanf_s("%f\n", &n1);
+
+	printf_s("Print number n2: ");
+	scanf_s("%f", &n2);
+
+	result = n2 == 0 ? 1 : (n1 / n2);
+	printf("Result: %f\n", &result);
+
+	return 0;
+}
