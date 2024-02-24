c0f2a817530ba9e037b72dd17e37847bb6e3ad2e
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-02-24 14:22:24 +0300

    Add files via upload

---
+++ Count the distance.cpp
@@ -0,0 +1,21 @@
+﻿	#include <stdio.h>
+
+	int distance(int time, int speed);
+	void printres(int res);
+
+	int main() {
+		distance(4, 9);
+		return 0;
+	}
+
+	int distance(int time, int speed) {
+		int res = time * speed;
+		printres(res);
+		return res;
+	}
+
+	void printres(int res) {
+		res == 1 ?
+			printf("Travel: 1 km \n") :
+			printf("Travel: %d km\n", res);
+	}
---
+++ divide 3 numbers or return 0.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+float dividef(float a, float b, float c);
+
+int main() {
+	float result = dividef(300, 7, 2);
+	printf("%.2f", result);
+
+	return 0;
+}
+
+float dividef(float a, float b, float c) {
+	if (a == 0 || b == 0 || c == 0) {
+		return 0;
+	}
+	else {
+		float res = a / b / c;
+		return res;
+	}
+	
+}
