d2bffe098b832c171031f0f67eedd57e71978d13
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-02-27 01:16:44 +0300

    Add files via upload

---
+++ PC struct.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+
+struct pc {
+	char cpu[40];
+	float weight;
+	int worktime;
+	int manycores;
+	char nameos[50];
+};
+
+
+int main() {
+	struct pc Windows = {"Dual Core CPU", 15.4f, 700, 4, "Windows"};
+	printf("CPU type: %s\nWeight of PC: %f kg\nTime of Work: %d min\nNumber of cores: %d\nName of OC: %s\n", Windows.cpu, Windows.weight, Windows.worktime, Windows.manycores, Windows.nameos);
+
+	return 0;
+}
---
+++ Tree struct.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+struct Tree {
+	int height;
+	char name[20];
+	int age;
+	int width;
+};
+
+int main() {
+
+	struct Tree Cedar;
+	Cedar.height = 100;
+	strcpy(Cedar.name, "Cedar");
+	Cedar.age = 20;
+	Cedar.width = 15;
+
+	struct Tree Cherry;
+	Cherry.height = 50;
+	strcpy(Cherry.name, "Cherry");
+	Cherry.age = 5;
+	Cherry.width = 10;
+
+	struct Tree Ficus = {300, "Ficus", 7, 100};
+	struct Tree Ficus2 = {500, "Ficus2", 10, 400};
+
+	printf("Age of %s tree: %d\n", Cherry.name, Cherry.age);
+	printf("Width of %s tree: %d\n", Cedar.name, Cedar.width);
+
+	return 0;
+}
