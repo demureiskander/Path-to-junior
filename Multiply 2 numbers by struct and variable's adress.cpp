bea27dd55d36f2477a20d2b856f5843347c63c22
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-02-29 21:52:03 +0300

    Add files via upload

---
+++ Multiply 2 numbers by struct and variable's adress.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+struct proportion {
+	int width, height;
+};
+
+void multiply(struct proportion *sizes);
+
+int main() {
+	struct proportion sizes = {5, 7};
+	multiply(&sizes);
+
+	return 0;
+}
+
+void multiply(struct proportion *sizes) {
+	int res = sizes->width * sizes->height;
+	printf("Result of multiply: %d\n", res);
+}
