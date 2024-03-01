ac4c7a1f14a88f1d8d166afbbf2df1b2a2fd1519
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-03-01 18:23:11 +0300

    Add files via upload

---
+++ Sum by struct and pointer.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+struct Pair {
+    int first;
+    int second;
+};
+
+int sum(struct Pair *pmyNumbers) {
+    int res = pmyNumbers->first + pmyNumbers->second;
+    return res;
+}
+
+int main() {
+    struct Pair myNumbers = {5, 10};
+    struct Pair *pmyNumbers = &myNumbers;
+
+    int sumResult = sum(pmyNumbers);
+
+    printf("Sum Result: %d\n", sumResult);
+    return 0;
+}
