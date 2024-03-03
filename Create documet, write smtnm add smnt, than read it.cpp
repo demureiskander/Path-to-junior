f9c23924fd83eb2ceb5850f2a449db37ae9f191d
Iskender Annamuhammedov <147938590+demureiskander@users.noreply.github.com>
2024-03-03 12:12:57 +0300

    Add files via upload

---
+++ Create documet, write smtnm add smnt, than read it.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	FILE* file = fopen("testfile.txt", "w");
+	fprintf(file, "Hello world!\n");
+	fclose(file);
+
+	file = fopen("testfile.txt", "a");
+	fprintf(file, "What's up?\n");
+	fclose(file);
+
+	char readfile[100];
+	file = fopen("testfile.txt", "r");
+	fgets(readfile, 100, file);
+	printf("%s", readfile);
+	fgets(readfile, 100, file);
+	printf("%s", readfile);
+	fclose(file);
+
+	return 0;
+}
---
+++ Get user input and paste to new document.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	char userinput[100];
+
+	printf("Write something: ");
+	scanf("%s", &userinput);
+
+	FILE *file = fopen("goshadz.txt", "w");
+	fprintf(file, ("%s", userinput));
+
+	fclose(file);
+
+	return 0;
+}
